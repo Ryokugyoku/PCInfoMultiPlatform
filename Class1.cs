d58acaf3768f871a97b6917db219812b5482de5d
Ryokugyoku <mashumaro1997@icloud.com>
2024-10-06 08:55:46 +0900

    first commit

---
+++ Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace PCInfoMultiPlatform;
+
+public class Class1
+{
+
+}
---
+++ PCInfoMultiPatform.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace PCInfoMultiPatform.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
